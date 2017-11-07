28f968b240d766c4498fd1de89dc0c6b49353cdc
Patrick Tone <pt@patrickt.one>
2017-11-07 06:29:07 -0600

    added tests

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 
 const fs = require('fs');
 const makeDir = require('make-dir');
+const path = require('path');
 
 module.exports = async(buffer, filePath) => {
-    await makeDir(filePath);
+    if (!buffer || !Buffer.isBuffer(buffer)) {
+        throw new TypeError('A buffer is required.');
+    }
+
+    if (!filePath) {
+        throw new TypeError('A file path is required.');
+    }
+
+    const dir = path.dirname(filePath);
+
+    await makeDir(dir);
 
     const err = await new Promise((resolve, reject) => fs.writeFile(filePath, buffer, (err) => {
         if (err) return reject(err);
---
+++ test.js
@@ -0,0 +1,53 @@
+'use strict'
+
+import test from 'ava';
+import saveBuffer from './';
+
+test('returns undefined', async t => {
+    const buffer = Buffer.from('hello world');
+    const err = await saveBuffer(buffer, './files/test.txt');
+
+    t.is(err, undefined);
+});
+
+test('rejects directory instead of file', async t => {
+    try {
+        const buffer = Buffer.from('hello world');
+        await saveBuffer(buffer, './files/');
+        t.fail();
+    } catch (err) {
+        t.true(err instanceof Error);
+        t.true(err.message.includes('illegal operation on a directory'));
+    }
+});
+
+test('rejects undefined buffer', async t => {
+    try {
+        const err = await saveBuffer(undefined, './files/test.txt');
+        t.fail();
+    } catch (err) {
+        t.true(err instanceof TypeError);
+        t.is(err.message, 'A buffer is required.');
+    }
+});
+
+test('rejects string instead of a buffer', async t => {
+    try {
+        const err = await saveBuffer('', './files/test.txt');
+        t.fail();
+    } catch (err) {
+        t.true(err instanceof TypeError);
+        t.is(err.message, 'A buffer is required.');
+    }
+});
+
+test('rejects string instead of a buffer', async t => {
+    try {
+        const buffer = Buffer.from('hello world');
+        const err = await saveBuffer(buffer);
+        t.fail();
+    } catch (err) {
+        t.true(err instanceof TypeError);
+        t.is(err.message, 'A file path is required.');
+    }
+});
